999bc404cd8c814f1696e214fe85c9d1a9b5db5b
UrielCruz <42217268+urielcruzrivera@users.noreply.github.com>
2024-06-12 19:28:40 +0000

    Revision de dependencias

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,25 @@
-// Placeholder to host a component
+import React from "react";
+
+const Hours = () => {
+    const shelterHours = [
+        { day: "Monday", open: "10:00", close: "16:00" },
+        { day: "Tuesday", open: "10:00", close: "16:00" },
+        { day: "Wednesday", open: "10:00", close: "16:00" },
+        { day: "Thursday", open: "10:00", close: "16:00" },
+        { day: "Friday", open: "10:00", close: "16:00" },
+        { day: "Saturday", open: "10:00", close: "16:00" },
+        { day: "Sunday", open: "09:00", close: "10:00" },
+    ];
+
+    return (
+        <div>
+            {shelterHours.map((hour, index) => (
+                <div key={index}>
+                    <p>{hour.day}: {hour.open} - {hour.close}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Hours;
